f77f13a6081e75f6054526f07ed92a45e509b6b8
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-10-08 17:10:21 +0200

    improve metadata and change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,16 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'JS Mastery',
   description: 'JS Mastery Resources',
+  other:{
+    "theme-color": "#0d1117",
+    "color-scheme": "dark only",
+    "twittter:image": "https://cdn.pixabay.com/photo/2018/08/18/13/26/interface-3614766_960_720.png",
+    "twitter:card": "summary_large_image",
+    "og:url": "jsmastery.pro",
+    "og:image": "https://cdn.pixabay.com/photo/2018/08/18/13/26/interface-3614766_960_720.png",
+    "og:type": "website",
+    
+  }
 }
 
 export default function RootLayout({
